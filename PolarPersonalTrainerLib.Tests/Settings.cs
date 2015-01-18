822606ea748f4fd0ff77f5ee53566530f7e3dded
Németh Gábor <gabor.nemeth1982@gmail.com>
2015-01-18 19:13:06 +0100

    fix date formatting in export, added test assembly

---
+++ PolarPersonalTrainerLib.Tests/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolarPersonalTrainerLib.Tests
+{
+    public static class Settings
+    {
+        /// <summary>
+        /// Name of the user
+        /// </summary>
+        public static string UserName { get; private set; }
+        /// <summary>
+        /// Password
+        /// </summary>
+        public static string Password { get; private set; }
+
+        static Settings()
+        {
+            UserName = ConfigurationManager.AppSettings["username"];
+            Password = ConfigurationManager.AppSettings["password"];
+        }
+    }
+}
