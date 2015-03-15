8060bb731d5b6434a10fa4d0bb5455c8278754f2
Anders Gjermshus <anders.gjermshus@onestopreporting.com>
2015-03-15 01:29:15 +0100

    Added support for heart beats

--- PolarPersonalTrainerLib/PPTExercise.cs
+++ PolarPersonalTrainerLib/PPTExercise.cs
@@ -13,6 +13,8 @@ namespace PolarPersonalTrainerLib
         public int maximum = 0;
         public int vo2Max = 0;
 
+        public List<HeartBeat> HeartBeats { get; set; }
+
         public Boolean hasData()
         {
             if (resting <= 0 && average <= 0 && maximum <= 0 && vo2Max <= 0)
@@ -29,5 +31,25 @@ namespace PolarPersonalTrainerLib
         public int calories { get; set; }
         public TimeSpan duration { get; set; }
         public HeartRate heartRate { get; set; }
+        /// <summary>
+        /// Gets or sets the distance for the excercise
+        /// </summary>
+        public double Distance { get; set; }
+
+        /// <summary>
+        /// Name of the exercise
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Indicates that the excercise has gps data available for export from PPT.com
+        /// </summary>
+        public bool HasGPSData { get; set; }
+    }
+
+    public class HeartBeat
+    {
+        public int HeartRate { get; set; }
+        public DateTime Time { get; set; }
     }
 }
